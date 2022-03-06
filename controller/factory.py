adf7ab261dbf76c0eb6e661a5cea6d7078e827d8
jouad01 <jouardiouardi@cifpfbmoll.eu>
2022-03-06 19:31:16 +0100

    feat: Creacion logica factory e items

--- controller/factory.py
+++ controller/factory.py
@@ -1,2 +1,21 @@
+from unicodedata import name
 from flask import Flask
 from flask_restful import Resource, Api
+from controller.items import Items
+from controller.objects import Objeto
+from controller.wellcome import Wellcome
+
+def creacion_app():
+    app = Flask(__name__)
+    
+    api = Api(app, catch_all_404s=True)
+
+    api.add_resource(Wellcome, "/")
+    api.add_resource(Items, "/")
+    api.add_resource(Objeto, "/")
+
+    return app
+
+if __name__ == '__main__':
+    app = creacion_app()
+    app.run(debug=True)
---
+++ controller/items.py
@@ -0,0 +1,7 @@
+import imp
+from flask_restful import Resource
+from services.services import Service
+
+class Items(Resource):
+    def get(self, name):
+        return Service.get_item(name), 200
