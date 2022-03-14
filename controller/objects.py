bac28885d6a20220379aff9c46294d74fbc66fa2
jouad01 <jouardiouardi@cifpfbmoll.eu>
2022-03-14 13:54:34 +0100

    refactor: Eliminados ficheros factory y objects

--- controller/objects.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource
-from services.services import Service
-
-class Objeto(Resource):
-    
-    def get(self, name):
-        return Service.get_objeto(name), 200  #Ping?¿
